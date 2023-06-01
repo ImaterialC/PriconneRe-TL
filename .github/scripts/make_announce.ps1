68d7dc87ae7e6ac8a3d8bb7c812c879fe80e5e3e
Thái Tuấn <taongo10x@gmail.com>
2023-06-01 16:40:38 +0700

    Fix changelog and download button

--- .github/scripts/make_announce.ps1
+++ .github/scripts/make_announce.ps1
@@ -1,17 +1,16 @@
 $Changelog = Get-Content -Raw ./RELEASE_NOTE
 
-if ($Changelog -match "Changelog") {
-    $Changelog = $Changelog.Remove(0, $Changelog.IndexOf("Changelog"))
+if ($Changelog -match "## Changelog") {
+    $Changelog = $Changelog.Remove(0, $Changelog.IndexOf("## Changelog"))
 
     $Replacement = @{
         "<summary>"  = "";
         "</summary>" = "";
         "<details>"  = "";
         "</details>" = "";
-        "Text"       = "Text:";
-        "Texture"    = "Texture:";
-        "Other"      = "Other:";
-        "-"          = "•"
+        "Text"       = "### Text:";
+        "Texture"    = "### Texture:";
+        "Other"      = "### Other:";
     }
     ($Replacement | ConvertTo-Json | ConvertFrom-Json).PSObject.Properties.Name | ForEach-Object {
         $Changelog = $Changelog.Replace($_, $Replacement.$_)
@@ -21,7 +20,7 @@ if ($Changelog -match "Changelog") {
     if ($Changelog.Length -ge 500) {
         $Changelog = $Changelog.Substring(0, 500)
         $Changelog += "..."
-        $Changelog += "*Check detailed changelog at the link above!*"
+        $Changelog += "`n*Check detailed changelog at the link below!*"
     }
 }
 else {
