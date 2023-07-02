646d1c9af487c314926b84ef50dbe715422cb896
Thái Tuấn <taongo10x@gmail.com>
2023-07-02 22:59:22 +0700

    Update action scripts
    - Rewrite Discord announcement script
    - Trim redundant blank line in changelog

--- .github/scripts/make_announce.ps1
+++ .github/scripts/make_announce.ps1
@@ -1,30 +1,20 @@
-$Changelog = Get-Content -Raw ./RELEASE_NOTE
-
-if ($Changelog -match "## Changelog") {
-    $Changelog = $Changelog.Remove(0, $Changelog.IndexOf("## Changelog"))
-
-    $Replacement = @{
-        "<summary>"  = "";
-        "</summary>`n" = "";
-        "`n<details>"  = "";
-        "`n</details>`n" = "";
-        "Text"       = "";
-        "Texture"    = "";
-        "Other"      = "";
+$Changelog = [System.Collections.ArrayList](Get-Content ./RELEASE_NOTE)
+if ($Changelog.Contains("## Changelog")) {
+    $Changelog.RemoveRange(0, $Changelog.IndexOf("## Changelog"))
+    for ($i = 0; $i -lt $Changelog.Count; $i++) {
+        if ($Changelog[$i] -match "<(\w|\/)+>") {
+            $Changelog[$i] = $null
+        }
     }
-    ($Replacement.PSOBject.Properties | Where-Object Name -eq "Keys").Value | ForEach-Object {
-        $Changelog = $Changelog.Replace($_, $Replacement.$_)
-        Write-Output "Replace $_ => $($Replacement.$_)"
-    }
+    $Changelog = [System.Collections.ArrayList]($Changelog | Where-Object { $_ -and $_.Trim() })
 
-    if ($Changelog.Length -ge 300) {
-        $Changelog = $Changelog.Substring(0, 300) #.Substring(0, $Changelog.LastIndexOf("`n"))
-        $Changelog += "..."
-        $Changelog += "`n*Check detailed changelog at the link below!*"
+    if ($Changelog.Count -ge 15) {
+        $Changelog.RemoveRange(15, $Changelog.Count - 15)
+        $null = $Changelog.Add("...`n*Check detailed changelog at the link below!*")
     }
 }
 else {
     $Changelog = ""
 }
-
-$Changelog >> "./DISCORD_NOTE"
+Write-Output "::group::Final Discord Message`n$Changelog`n::endgroup::`n"
+Set-Content -Path ./DISCORD_NOTE -Value $Changelog
